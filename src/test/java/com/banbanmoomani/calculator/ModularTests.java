916ec096d22100c19519e1848fb3678df3336019
lxxsxynnn <blip0819@gmail.com>
2024-06-18 16:29:53 +0900

    나머지 구하기 기능 개발 완료(#2)
    첫 번째 수를 두 번째 수로 나눈 나머지를 구하는 함수를 테스트코드와 함께 개발 완료함

---
+++ src/test/java/com/banbanmoomani/calculator/ModularTests.java
@@ -0,0 +1,37 @@
+package com.banbanmoomani.calculator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ModularTests {
+    @Test
+    @DisplayName("4를 3으로 나눈 나머지가 1인지 확인")
+    public void testMod(){
+
+        //given
+        int first = 4;
+        int second =3;
+
+        //when
+        int result = new Modular().mod(first, second);
+
+        //then(verify)
+        Assertions.assertEquals(1, result);
+    }
+
+    @Test
+    @DisplayName("13을 8로 나눈 나머지가 5인지 확인")
+    public void testMod2(){
+
+        //given
+        int first = 13;
+        int second =8;
+
+        //when
+        int result = new Modular().mod(first, second);
+
+        //then(verify)
+        Assertions.assertEquals(5, result);
+    }
+}
